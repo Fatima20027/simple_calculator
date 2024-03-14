fd4ffe54a9836e9468fcfa7736db393c9d80f7f3
Fatima20027 <fatima.hailou@icloud.com>
2024-03-14 23:14:20 +0000

    writing functions

---
+++ my_calculatoor/calcul_fun.py
@@ -0,0 +1,23 @@
+def add(lst):
+    return sum(lst)
+
+def subtract(lst):
+    result = lst[0]
+    for num in lst[1:]:
+        result -= num
+    return result
+
+def multiply(lst):
+    result = 1    #we want to multiply all numbers
+    for num in lst:
+        result *= num
+    return result
+
+def divide(lst):
+    result = lst[0]
+    for num in lst[1:]:
+        if num != 0:
+            result /= num
+        else:
+            return "Error: Division by zero"
+    return result
