b40a34f1ad7479c33ecc5ad0bd6cf92b7e5bd48b
Fatima20027 <fatima.hailou@icloud.com>
2024-03-15 22:05:29 +0000

    quick change

--- calcul_1.py
+++ calcul_1.py
@@ -1,4 +1,24 @@
-from calcul_fun.py import add, sub, mul, div
+#from calcul_fun import add, sub, mul, div
 while True:
     print("WELCOME TO MY CALCULATION")
     user = float(input("Enter a mathematical expression: "))
+    if user == '+':
+        result = sum(numbers)
+    elif user == '-':
+        result = lst[0] - sum(lst[1:])
+    elif user == '*':
+        result = 1
+        for num in lst:
+            result *= num
+    elif user == '/':
+        if 0 in lst[1:]:
+            print("Error: Division by zero!")
+            continue
+        result = lst[0]
+        for num in lst[1:]:
+            result /= num
+    else:
+        print("Invalid operator.")
+        continue
+
+    print("Result:", result)
--- my_calculatoor/calcul_fun.py
+++ my_calculatoor/calcul_fun.py
@@ -21,3 +21,4 @@ def divide(lst):
         else:
             return "Error: Division by zero"
     return result
+    
